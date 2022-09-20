ff036d24620e81ef4da83e427625f500ce1cffe0
DESKTOP-LFV1CN5\USER <jungsoo>
2022-09-20 23:28:09 +0900

    게시판 데이터 추가 기능 구현 완료

--- src/main/java/com/test/board/controller/BoardController.java
+++ src/main/java/com/test/board/controller/BoardController.java
@@ -1,14 +1,29 @@
 package com.test.board.controller;
 
+import com.test.board.entity.Board;
+import com.test.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
     @GetMapping("/login")
     public String boardWriteForm(){
 
         return "login";
+    }
+
+
+    @PostMapping("/board/writedopro")
+    public String boardWritePro(Board board){
+
+        boardService.write(board);
+
+        return "";
     }
 }
