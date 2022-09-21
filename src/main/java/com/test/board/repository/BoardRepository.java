beb09266598ff0ea7c2a9d33a29f32ec54420d08
DESKTOP-LFV1CN5\USER <jungsoo>
2022-09-21 22:46:08 +0900

    게시판 추가된 데이터 리스트로 뽑기

---
+++ src/main/java/com/test/board/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.test.board.repository;
+
+import com.test.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board,Integer> {
+}
