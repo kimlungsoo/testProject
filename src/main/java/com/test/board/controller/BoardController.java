0f62bd3485e56eb85d7c81ea6857cde2331972ec
DESKTOP-LFV1CN5\USER <jungsoo>
2022-09-28 00:05:35 +0900

    ResponseBody 사용해보기

--- src/main/java/com/test/board/controller/BoardController.java
+++ src/main/java/com/test/board/controller/BoardController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
@@ -34,4 +36,27 @@ public class BoardController {
         model.addAttribute("list",boardService.boardList());
         return "boardlist";
     }
+
+    @GetMapping("test-api")
+    @ResponseBody // 바디 부분에 내가 직접 넣어 주겠다.
+    public TestName test(@RequestParam("name") String name) {
+        TestName testName = new TestName();
+        System.out.println(name);
+        testName.setName(name);
+        return testName;
+
+    }
+        static class TestName {
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            private String name;
+
+        }
+
 }
